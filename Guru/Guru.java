a6eaa139fbd9e3ca8045fee8a382af2f5fbcfa3b
ElangDipa <elang_purwadhiyaksa_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:36:36 +0700

    Add files via upload

---
+++ Guru/Guru.java
@@ -0,0 +1,16 @@
+public class Guru {
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    public Guru() {
+        id = 0;
+        nama = "";
+        mapel = "";
+        alamat = "";
+    }
+    
+
+    
+}
---
+++ Guru/TesGuru.java
@@ -0,0 +1,65 @@
+public class TesGuru {
+    public static void main(String[] args) {
+        Guru aulia = new Guru();
+        Guru pasha = new Guru();
+        Guru diaur = new Guru();
+        Guru feni = new Guru();
+        Guru agus= new Guru();
+
+        aulia.id = 1;
+        aulia.nama = "Aulia Mas'adah";
+        aulia.mapel = "Produktif RPL";
+        aulia.alamat = "Malang";
+
+        pasha.id = 2;
+        pasha.nama = "Pashatania Fitri Indah Lestari";
+        pasha.mapel = "Produktif RPL";
+        pasha.alamat = "Malang";
+
+        diaur.id = 3;
+        diaur.nama = "Diaur Rahman";
+        diaur.mapel = "Penjaskes & IPAS";
+        diaur.alamat = "Ngawi";
+
+        feni.id = 4;
+        feni.nama = "Feniliya Mayrini";
+        feni.mapel = "Matematika";
+        feni.alamat = "Malang";
+
+        agus.id = 5;
+        agus.nama = "Bagus Arifin";
+        agus.mapel = "Informatika";
+        agus.alamat = "Bogor";
+
+        System.out.println("Ini Data Bu Aulia");
+        System.out.println("ID : "+aulia.id);
+        System.out.println("Nama: "+aulia.nama);
+        System.out.println("Mapel :"+aulia.mapel);
+        System.out.println("Alamat :"+aulia.alamat);
+        System.out.println("Ini Data Bu Pasha");
+        System.out.println("ID : "+pasha.id);
+        System.out.println("Nama: "+pasha.nama);
+        System.out.println("Mapel :"+pasha.mapel);
+        System.out.println("Alamat :"+pasha.alamat);
+        System.out.println("Ini Data Pak Diaur");
+        System.out.println("ID : "+diaur.id);
+        System.out.println("Nama: "+diaur.nama);
+        System.out.println("Mapel :"+diaur.mapel);
+        System.out.println("Alamat :"+diaur.alamat);
+        System.out.println("Ini Data Bu Feni");
+        System.out.println("ID : "+feni.id);
+        System.out.println("Nama: "+feni.nama);
+        System.out.println("Mapel :"+feni.mapel);
+        System.out.println("Alamat :"+feni.alamat);
+        System.out.println("Ini Data Pak Bagus");
+        System.out.println("ID : "+agus.id);
+        System.out.println("Nama: "+agus.nama);
+        System.out.println("Mapel :"+agus.mapel);
+        System.out.println("Alamat :"+agus.alamat);
+        
+
+
+
+
+    }
+}
